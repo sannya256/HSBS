00e80f345e5625e8eebe1adf0413f59e86d97577
Harry <harrymithun10@gmail.com>
2021-03-20 08:00:39 +0000

    Patient Medical History Added

--- planetdoctor.js
+++ planetdoctor.js
@@ -56,3 +56,34 @@ exports.Pharmacist = class {
     this.Hospital_no = Hospital_no;
   }
 }
+
+exports.Patient_Medical_History = class {
+  // patienet id
+  Patient_ID;
+  // patient medical history
+  P_MedicalHistoty;
+  // patient symptoms 
+  P_Symptoms;
+  // patient reports 
+  P_Reports;
+  // patient prescriptions
+  P_Prescriptions;
+  // patient treatment
+  P_Treatment;
+  // Doctor ID
+  Doctor_ID;
+  // pharmacist ID
+  Pharmacist_ID;
+
+
+  constructor(Patient_ID, _P_MedicalHistory,P_Symptoms, P_Reports, P_Prescriptions, P_Treatment, Doctor_ID, Pharmacist_ID) {
+    this.Patient_ID = Patient_ID;
+    this.P_MedicalHistory = P_MedicalHistoty;
+    this.P_Symptoms = P_Symptoms;
+    this.P_Reports = P_Reports;
+    this.P_Prescriptions = P_Prescriptions;
+    this.P_Treatment = P_Treatment;
+    this.Doctor_ID = Doctor_ID;
+    this.Pharmacist_ID = Pharmacist_ID; 
+  }
+}
