123d66fd8e1aec01a0a16d647366fa75d032dbd1
Harry <harrymithun10@gmail.com>
2021-03-20 15:21:11 +0000

    Diagnosis class added

--- planetdoctor.js
+++ planetdoctor.js
@@ -1,89 +1,28 @@
 "use strict";
 
-exports.Patient = class {
+exports.Diagnosis = class {
   // Patient ID
-  id;
+  P_ID;
   // Patient first name
-  first_name;
-  // Patient surname
-  last_name;
-  //Gender
-  gender;
-  // Date of Birth
-  dob;
+  P_First_Name;
+  // Patient last name 
+  P_Last_Name;
+  //Diagnostics
+  Diagnostics;
+  // Medications
+  Medications;
+  // Tests
+  Tests;
+  // Referal
+  Referal;
 
-  constructor(id, first_name, last_name, gender,dob ) {
-    this.id = id;
-    this.first_name = first_name;
-    this.last_name = last_name;
-    this.gender = gender;
-    this.dob = dob; 
-  }
-}
-
-exports.Doctor = class {
-  // doctors id
-  ID;
-  // doctors name
-  Name;
-  // doctors speciality modules
-  Speciality;
-
-  //Doctor hospital no.
-  Hospital_no;
-  
-
-  constructor(ID, Name, Speciality, Hospital_no) {
-    this.ID = ID;
-    this.Name = Name;
-    this.Speciality = Speciality;
-    this.Hospital_no = Hospital_no;
-  }
-}
-
-exports.Pharmacist = class {
-  // pharmacist id
-  ID;
-  // pharmacist name
-  Name;
-  // pharmacist hospital no.
-  Hospital_no;
-
-
-  constructor(ID, Name, Hospital_no) {
-    this.ID = ID;
-    this.Name = Name;
-    this.Hospital_no = Hospital_no;
-  }
-}
-
-exports.Patient_Medical_History = class {
-  // patienet id
-  Patient_ID;
-  // patient medical history
-  P_MedicalHistoty;
-  // patient symptoms 
-  P_Symptoms;
-  // patient reports 
-  P_Reports;
-  // patient prescriptions
-  P_Prescriptions;
-  // patient treatment
-  P_Treatment;
-  // Doctor ID
-  Doctor_ID;
-  // pharmacist ID
-  Pharmacist_ID;
-
-
-  constructor(Patient_ID, _P_MedicalHistory,P_Symptoms, P_Reports, P_Prescriptions, P_Treatment, Doctor_ID, Pharmacist_ID) {
-    this.Patient_ID = Patient_ID;
-    this.P_MedicalHistory = P_MedicalHistoty;
-    this.P_Symptoms = P_Symptoms;
-    this.P_Reports = P_Reports;
-    this.P_Prescriptions = P_Prescriptions;
-    this.P_Treatment = P_Treatment;
-    this.Doctor_ID = Doctor_ID;
-    this.Pharmacist_ID = Pharmacist_ID; 
+  constructor(P_ID, P_First_Name, P_Last_Name, Diagnostics, Medications, Tests, Referal) {
+    this.P_ID = P_ID;
+    this.P_First_Name = P_First_Name;
+    this.P_Last_Name = P_Last_Name;
+    this.Diagnostics = Diagnostics;
+    this.Medications = Medications;
+    this.Tests = Tests;
+    this.Referal = Referal;
   }
 }
