739f6c54aadeb6f2fed56937904154965f79970c
suzana-roe <svzana07@gmail.com>
2021-03-20 15:39:38 +0000

    Patient class created

--- planetdoctor.js
+++ planetdoctor.js
@@ -25,4 +25,32 @@ exports.Diagnosis = class {
     this.Tests = Tests;
     this.Referal = Referal;
   }
+} 
+exports.Patients = class {
+  // Patient ID
+  P_ID;
+  // Patient first name
+  P_First_Name;
+  // Patient last name 
+  P_Last_Name;
+  //Date of birth
+  DOB;
+  //Gender of patients
+  Gender;
+  //Diagnostics
+  Diagnostics;
+  // Medications
+  Medications;
+ 
+
+  constructor(P_ID, P_First_Name, P_Last_Name, DOB, Gender, Diagnostics, Medications) {
+    this.P_ID = P_ID;
+    this.P_First_Name = P_First_Name;
+    this.P_Last_Name = P_Last_Name;
+    this.DOB = DOB;
+    this.Gender= Gender;
+    this.Diagnostics = Diagnostics;
+    this.Medications = Medications;
+  }
 }
+    
