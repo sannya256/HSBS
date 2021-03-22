bd9aaf01ffd57de545064ed1c5baba8d3f7a89f9
Sandra <sandra.nyaguti@hotmail.com>
2021-03-22 20:08:20 +0000

    included app.js

--- iha.js
+++ iha.js
@@ -1,3 +1,17 @@
-// This is a tester code
+// To use or call the app.js file 
+"use strict";
 
-console.log("Hello world!, this is the HSBS App dev team");
+// This code will import app.js into iha.js and run the code in app.js
+const app = require("./app/app");
+
+
+
+
+
+
+
+
+// This is a tester code
+//this code appears after running npm start = everything working as expected
+
+//console.log("Hello world!, this is the HSBS App dev team");
