d3f2b4b95d5af8b5e91fb76b35bebc3a578c08b0
osheab <osheab@roehampton.ac.uk>
2021-03-23 09:35:15 +0000

    data volunteering

--- data/data.js
+++ data/data.js
@@ -4,10 +4,10 @@
 const sqlite3 = require("sqlite3").verbose();
 
 // This code intructs the application layer to use planetdoctor classes
-const planetdocotor = require("../planetdoctor.js");
+const planetdoctor = require("../planetdoctor.js");
 
 // This code will connect to the planetdocotor database.
-var db = new sqlite3.Database("data/planetdoctor.db", function(err) {
+var db = new sqlite3.Database("data/planetDoctor.db", function(err) {
     // this code will run in case of an error
     if (err) {
         return console.error(err.message);
@@ -15,3 +15,27 @@ var db = new sqlite3.Database("data/planetdoctor.db", function(err) {
     // This code will run if there is no error
     console.log("You are now Connected to planetdoctor database.");
 });
+
+// Export getProgrammes function
+exports.getVolunteers = function(callback) {
+    // Create SQL statement
+    var sql = `SELECT * FROM volunteers`;
+    // Execute query. Return all
+    db.all(sql, function(err, rows) {
+        // Check if error
+        if (err) {
+            return console.error(err.message);
+        }
+        // Create programme array
+        var volunteering = [];
+        // Loop through rows creating programme objects
+        for (var row of rows) {
+            // Create programme object
+            var volunt = new planetdoctor.Volunteers(row.ID, row.First_Name, row.Last_Name, row.Nationality, row.camp_loc);
+            // Add object to array
+            volunteering.push(volunt);
+        }
+        // Execute callback function
+        callback(volunteering);
+    });
+};
