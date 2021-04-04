e29d43d0b3330b999594d5493b43608b7c4b7c44
suzana-roe <svzana07@gmail.com>
2021-04-04 20:14:38 +0100

    Added patient to front end planetdoctor.js

--- Static/planetdoctor.js
+++ Static/planetdoctor.js
@@ -30,3 +30,28 @@ constructor(Patient_ID, P_First_Name, P_Last_Name, Diagnosis, Drug_ID, Drug_name
     this.Referal = Referal;
     }
 }
+
+class Patients {
+  // This is Patient's ID number
+  Patient_ID;
+  // This is Patient's first name
+  P_First_Name;
+  // This is Patient's last name 
+  P_Last_Name;
+  //This is Patient's Date of birth
+  DOB;
+  //This is Patient's Gender
+  Gender;
+  //This will show Patient's current symptoms 
+  Symptoms;
+
+ 
+  constructor(Patient_ID, P_First_Name, P_Last_Name, DOB, Gender, Symptoms) {
+    this.Patient_ID = Patient_ID;
+    this.P_First_Name = P_First_Name;
+    this.P_Last_Name = P_Last_Name;
+    this.DOB = DOB;
+    this.Gender = Gender;
+    this.Symptoms = Symptoms;
+  }
+}
