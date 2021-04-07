76043b77ab8fe428dc9f1426b16cb0098e53fe73
Sandra <sandra.nyaguti@hotmail.com>
2021-04-07 22:28:55 +0100

    Add Delete button

--- Static/all_doctors.js
+++ Static/all_doctors.js
@@ -6,8 +6,21 @@ var  PDapp= angular.module("PDapp", []);
 // Code here will Create the doctor controller and request for doctors data from the backend
 
 PDapp.controller("doctorController", function($scope, $http) {
-    //This code will get doctors data from the backend
+    //This code will get doctors data from the backend and display to the frontend 
     $http.get('/doctors').then(function(response) {
         $scope.doctors = response.data;
     });
-}); 
+
+    // Sends a delete message to the server
+    $scope.deleteDoctor = function(Doctor_ID) {
+    // Send delete message to /module/code
+        $http.delete("/doctor/" + Doctor_ID).then(function(response) {
+      // When request completes, refresh list of modules
+            $http.get("/doctors").then(function(response) {
+                $scope.doctors = response.data;
+                });
+            });
+        };
+    });
+
+
