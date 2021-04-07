7d6b9874761240ab6ed24d04b8e262512098f4b7
Harry <harrymithun10@gmail.com>
2021-04-07 01:22:52 +0100

    made changes to diagnostics.js

--- Static/all_diagnostics.js
+++ Static/all_diagnostics.js
@@ -3,5 +3,5 @@
 var mainApp = angular.module("mainApp", []);
 
 mainApp.controller("diagnosticsController", function($scope, $http) {
-    $scope.diagnostic = new diagnostic("000", "Test", "random1", "random2","random3", "random4", "random5", "random6");
+    $scope.diagnostics = new Diagnostic("000", "Test", "random1", "random2", "random3", "random4", "random5", "random6");
 });
