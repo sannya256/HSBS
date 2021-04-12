6e4d33703de1a29fd00e071e3aa53b760927f9d4
Harry <harrymithun10@gmail.com>
2021-04-12 12:35:16 +0100

    made minor changes

--- Static/all_diagnostics.js
+++ Static/all_diagnostics.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var mainApp = angular.module("PDApp", []);
+var PDApp = angular.module("PDApp", []);
 
 PDApp.controller("diagnosticController", function($scope, $http) {
     $http.get('/diagnostics').then(function(response) {
