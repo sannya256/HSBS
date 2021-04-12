dd31a2de83b50003c357d0177fd511221dbb94bb
Harry <harrymithun10@gmail.com>
2021-04-12 05:55:36 +0100

    made changes to endpoints and data.js

--- Static/all_diagnostics.js
+++ Static/all_diagnostics.js
@@ -4,6 +4,6 @@ var mainApp = angular.module("PDApp", []);
 
 PDApp.controller("diagnosticsController", function($scope, $http) {
     $http.get("/diagnostics").then(function(response) {
-        $scope.diagnostics = response.data;
+        $scope.diagnostic = response.data;
     });
 });
