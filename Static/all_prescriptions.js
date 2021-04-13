9b639ac4f9695e0deddf11da4bc4a4353c93292a
Harman2019 <harman_kang96@outlook.com>
2021-04-13 01:51:13 +0100

    changes to prescriptions js

--- Static/all_prescriptions.js
+++ Static/all_prescriptions.js
@@ -3,5 +3,7 @@
 var PDApp = angular.module("PDApp", []);
 
 PDApp.controller("prescriptionsController", function($scope, $http) {
-    $scope.prescrtiptions = new Prescriptions("000", "Test", "random1", "random2", "random3", "random4", "random5", "random6");
+    $http.get('/prescriptions').then(function(response) {
+        $scope.prescriptions = response.data;
+    });
 });
