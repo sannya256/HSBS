d93fc2d77c81b659a61917c9e2d5d013ec214123
Harry <harrymithun10@gmail.com>
2021-04-17 20:36:55 +0100

    added select and delete to diagnostics frontend

--- Static/all_diagnostics.js
+++ Static/all_diagnostics.js
@@ -1,9 +1,30 @@
 "use strict";
 
+//This code will get "PDApp" from the webpage
 var PDApp = angular.module("PDApp", []);
 
+//This code creates the dignostics controller and requests diagnostics data from the backend    
 PDApp.controller("diagnosticController", function($scope, $http) {
+    //document.getElementById("selected").style.display = "none"
+
     $http.get('/diagnostics').then(function(response) {
-        $scope.diagnostics = response.data;
+        $scope.diagnostics = response.data; //linking the diagnostics json data
     });
 });
+
+//The following code will add user interactions
+
+    $scope.selectDiagnostic = function(Patient_ID) {
+        $http.get("/disgnostic/" + Patient_ID).then(function(response) {
+            $scope.selectDiagnostic = response.data;
+            document.getElementById("selected").style.display = "block";
+        };
+    };
+
+    $scope.deleteDiagnostic = function(Patient_ID) {
+        $http.delete("/diagnostic/" + Patient_ID).then(function(response) {
+            $http.get("/diagnostics/").then(function(response) {
+                $scope.Diagnostics = response.data;
+            });
+        });
+    }; 
