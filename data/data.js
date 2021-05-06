084b2f5bd9aeebe568f1a212e33d3607d35f60aa
suzana-roe <svzana07@gmail.com>
2021-05-06 15:01:43 +0100

    Changes to data.js

--- data/data.js
+++ data/data.js
@@ -306,8 +306,8 @@ exports.getPatients = function(callback) {
 }; */
 
 
-//Adding  a deleteDoctor function
-// This code will delete a doctor from the database
+//Adding  a deletePatient function
+// This code will delete a patient from the database
 exports.deletePatient = function(Patient_ID, callback) {
     // SQL delete statement
     var sql = `DELETE FROM Patients WHERE Patient_ID='${Patient_ID}'`;
